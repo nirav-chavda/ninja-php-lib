76d9774880fc73b019cb47a478aa6e0d9ed82136
Nirav Chavda <nrvdchavda@gmail.com>
2021-10-01 01:22:30 +0530

    code optimization and docs added

--- src/Auth.php
+++ src/Auth.php
@@ -7,32 +7,53 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Auth {
 
+    /**
+     * user
+     * returns current authenticated user object
+     * @return object
+     */
     public static function user() {
         $user = Capsule::table('users')->where('id', Session::get('user_id'))->first();
         return $user;
     }
-    
-    public static function get($id) {
-        Session::get('user_id',$id);
-        Session::get('auth',true);
-    }
 
+    /**
+     * id
+     * returns current authenticated user's id
+     * @return string
+     */
     public static function id() {
         return Session::get('user_id');    
     }
 
+    /**
+     * set
+     * set used_id in session
+     * @return void
+     */
     public static function set($id) {
         Session::set('user_id',$id);
         Session::set('auth',true);
     }
 
+    /**
+     * guest
+     * check if user is guest or authenticated
+     * @return bool
+     */
     public static function guest() {
-        if(Session::has('auth'))
+        if(Session::has('auth')) {
             return 0;
-        else
+        } else {
             return 1;
+        }
     }
 
+    /**
+     * deAuth
+     * sign out the authenticated user
+     * @return void
+     */
     public static function deAuth() {
 
         if(Session::has('auth')) {
