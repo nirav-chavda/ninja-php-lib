ea2a21cb3069cdd5694fd0cd975ba53274cca044
Nirav Chavda <nrvdchavda@gmail.com>
2021-10-01 01:36:51 +0530

    replaced hardcoded die with exception

--- lib/Controller.php
+++ lib/Controller.php
@@ -17,12 +17,14 @@ class Controller
      */
     public function middleware($values, $except = '')
     {
-        if (gettype($values) == 'string')
+        if (gettype($values) == 'string') {
             $this->middlewares[0] = $values;
-        else
+        } else {
             $this->middlewares = $values;
-        if (isset($except['except']) && count($except['except']) > 0)
+        }
+        if (isset($except['except']) && count($except['except']) > 0) {
             $this->except = $except['except'];
+        }
     }
 
     /**
@@ -52,7 +54,7 @@ class Controller
         if (file_exists($root . "\\app\\views\\" . $view . '.php')) {
             require_once $root . "\\app\\views\\" . $view . '.php';
         } else {
-            die($view . ' view not found');
+            panic($view . ' view not found');
         }
     }
 }
